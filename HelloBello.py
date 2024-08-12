df0548d044ba2c1dee159167e705a9becd96f6ac
Orbia343 <orbia.lon3r4ng3r@gmail.com>
2024-08-12 11:20:15 +0530

    Committing Stashed changes

--- HelloBello.py
+++ HelloBello.py
@@ -1,2 +1,5 @@
+import random as dice
 print('Hello There! General Kenobi.')
 yourName = str(input('What is your name? '))
+print(f"Welcome Back, {yourName}")
+print(f"It's been {dice.randint(10,100)} days since you logged in!")
